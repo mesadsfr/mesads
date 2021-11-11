900b7a57928ed7e9a2d9add7b298a8482b04ecad
Julien Castets <castets.j@gmail.com>
2021-11-11 15:53:47 +0100

    Move templates to root folder

--- mesads/settings.py
+++ mesads/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'mesads.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'mesads/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
