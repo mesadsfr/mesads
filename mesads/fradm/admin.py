979251b9327f4d29293665dfcab7dc43b1856fa0
Julien Castets <castets.j@gmail.com>
2021-11-30 11:45:36 +0100

    Improve fradm admin

--- mesads/fradm/admin.py
+++ mesads/fradm/admin.py
@@ -3,25 +3,53 @@ from django.contrib import admin
 from .models import Commune, EPCI, Prefecture
 
 
+class ReadOnlyModelAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 @admin.register(Commune)
-class CommuneAdmin(admin.ModelAdmin):
+class CommuneAdmin(ReadOnlyModelAdmin):
     list_display = (
         'insee',
         'libelle',
     )
 
+    search_fields = (
+        'insee__istartswith',
+        'libelle',
+    )
+
 
 @admin.register(Prefecture)
-class PrefectureAdmin(admin.ModelAdmin):
+class PrefectureAdmin(ReadOnlyModelAdmin):
     list_display = (
         'numero',
         'libelle',
     )
 
+    search_fields = (
+        'numero__iexact',
+        'libelle',
+    )
+
+
 @admin.register(EPCI)
-class EPCIAdmin(admin.ModelAdmin):
+class EPCIAdmin(ReadOnlyModelAdmin):
     list_display = (
         'siren',
         'departement',
+        'name',
+    )
+
+    search_fields = (
+        'siren__startswith',
+        'departement__istartswith',
         'name',
     )
