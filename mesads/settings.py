eb2a3f846901d0168e21d65f958d59eaa8ecf68d
Julien Castets <castets.j@gmail.com>
2021-12-13 12:43:04 +0100

    Remove django-oauth-toolkit
    It can be useful to add it again in the future, but for now we do not
    provide an API.

--- mesads/settings.py
+++ mesads/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'oauth2_provider',
     'debug_toolbar',
     'django_registration',
 
--- mesads/urls.py
+++ mesads/urls.py
@@ -9,7 +9,6 @@ import debug_toolbar
 urlpatterns = [
     path('auth/', include('mesads.users.urls')),
     path('admin/', admin.site.urls),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('', include('mesads.app.urls')),
 
     path('__debug__/', include(debug_toolbar.urls)),
