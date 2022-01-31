5ddbbfa3e62484e6aedef4f8fd0b4f1c2fdf78d8
Julien Castets <castets.j@gmail.com>
2022-01-31 13:14:55 +0100

    Improve users admin

--- mesads/users/admin.py
+++ mesads/users/admin.py
@@ -1,10 +1,18 @@
+from django.utils.html import format_html
 from django.contrib import admin
 
 from .models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('email', 'admin_roles')
+
+    readonly_fields = ('date_joined', 'last_login',)
+    fields = ('date_joined', 'last_login', 'is_superuser', 'is_active',)
+
+    def admin_roles(self, user):
+        roles = user.adsmanageradministrator_set.all()
+        return format_html('<br />'.join(str(role) for role in roles))
 
 
 admin.site.register(User, UserAdmin)
