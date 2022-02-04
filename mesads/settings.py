34a4fac6bdbe7b3e73971df8a8bc6d1df6701bbb
Julien Castets <castets.j@gmail.com>
2022-02-04 19:22:05 +0100

    Setup postgresql

--- mesads/settings.py
+++ mesads/settings.py
@@ -105,8 +105,11 @@ WSGI_APPLICATION = 'mesads.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mesads',
+        'HOST': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'mesads',
     }
 }
 
