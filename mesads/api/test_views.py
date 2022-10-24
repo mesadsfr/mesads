3b292bed4178f71ce75db49c9a88c1bb0e6e7cba
Julien Castets <castets.j@gmail.com>
2022-10-24 14:53:40 +0200

    Remove api/test_views.py
    This file is pending work and should not have been committed yet.

--- mesads/api/test_views.py
+++
@@ -1,9 +0,0 @@
-from mesads.app.unittest import ClientTestCase
-
-from .views import ADSUpdatesViewSet
-
-
-class TestADSUpdatesViewSet(ClientTestCase):
-    def test_get_queryset(self):
-        queryset = ADSUpdatesViewSet().get_queryset()
-        print(queryset)
