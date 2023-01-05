f359976b8bfa1e45ecc8ceaf707d31f0aa664b8e
Julien Castets <castets.j@gmail.com>
2023-01-05 13:03:09 +0100

    Make sure production settings are removed when running tests

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mesads.settings')
+    if 'test' in sys.argv:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mesads.settings_test')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mesads.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ mesads/settings_test.py
@@ -0,0 +1,21 @@
+"""Test settings.
+
+Make sure to reset production data that might have been retrieved from the
+environment. We do not want to store files in S3 or send emails in tests.
+"""
+from .settings import *
+
+
+INSEE_TOKEN = None
+
+EMAIL_HOST = None
+EMAIL_PORT = None
+EMAIL_HOST_USER = None
+EMAIL_HOST_PASSWORD = None
+EMAIL_USE_TLS = None
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+AWS_S3_ENDPOINT_URL = None
+AWS_S3_ACCESS_KEY_ID = None
+AWS_S3_SECRET_ACCESS_KEY = None
+AWS_STORAGE_BUCKET_NAME = None
