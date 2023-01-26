bd94d1f992f4ed50a5cbaa503a9646e2ea149fd3
Julien Castets <castets.j@gmail.com>
2023-01-26 16:08:26 +0100

    Add noqa for import *

--- mesads/app/admin/__init__.py
+++ mesads/app/admin/__init__.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import Group
 from django.contrib import admin
 
-from .ads import *
-from .ads_manager import *
-from .ads_manager_administrator import *
-from .ads_manager_request import *
-from .ads_update_file import *
+from .ads import *  # noqa
+from .ads_manager import *  # noqa
+from .ads_manager_administrator import *  # noqa
+from .ads_manager_request import *  # noqa
+from .ads_update_file import *  # noqa
 
 # Remove "Group" administration from admin. We do not use groups in the
 # application.
