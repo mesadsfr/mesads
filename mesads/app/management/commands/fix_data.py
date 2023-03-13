f1be2e23e5cd05468e29fa1f22aec27b2aeed8ec
Julien Castets <castets.j@gmail.com>
2023-03-13 18:08:40 +0100

    Improve logging for temporary script fix_data

--- mesads/app/management/commands/fix_data.py
+++ mesads/app/management/commands/fix_data.py
@@ -9,7 +9,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for ads in ADS.objects.select_related('ads_manager').prefetch_related('adsuser_set').filter(ads_creation_date__gte=date(2014, 10, 1)):
             if ads.ads_manager.is_locked:
-                print(f'Oops, problem with ADS {ads.id} which belongs to a locked ADSManager.')
+
+                if (
+                    (ads.attribution_date and ads.attribution_date != ads.ads_creation_date)
+                    or ads.attribution_reason
+                    or ads.attribution_type
+                    or ads.transaction_identifier
+                ):
+                    print(f'Oops, unable to ADS {ads.id} created on {ads.ads_creation_date} which belongs to a locked ADSManager.')
+                    for field, value in (
+                        ('attribution_date', ads.attribution_date if ads.attribution_date != ads.ads_creation_date else None),
+                        ('attribtution_reason', ads.attribution_reason),
+                        ('attribution_type', ads.attribution_type),
+                        ('transaction_identifier', ads.transaction_identifier)
+                    ):
+                        if value:
+                            print(f'\t{field}: {value}')
                 continue
 
             # For all new ADS, attribution fields should be empty.
