38fbeefae06918019bf10ae666dfbe90635fd8ae
Julien Castets <castets.j@gmail.com>
2023-04-06 15:39:14 +0200

    Send django-crons exceptions to sentry

--- mesads/app/crons.py
+++ mesads/app/crons.py
@@ -1,6 +1,24 @@
+import functools
+from datetime import date
+
+from sentry_sdk import capture_exception
+
 from django.core.management import call_command
 
 from django_cron import CronJobBase, Schedule
+
+from mesads.app.models import ADS
+
+
+def sentry_exceptions(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            capture_exception(e)
+            raise
+    return inner
 
 
 class ImportDataForParis(CronJobBase):
@@ -9,5 +27,6 @@ class ImportDataForParis(CronJobBase):
 
     code = 'import_last_update_file_from_Paris'  # unique code to represent this cron job
 
+    @sentry_exceptions
     def do(self):
         call_command('import_last_update_file_from_paris')
