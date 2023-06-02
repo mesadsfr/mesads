a57c6b53ae292623a586dd85377b6da5affb98fc
Julien Castets <castets.j@gmail.com>
2023-06-02 17:07:15 +0200

    Add selectattr template

---
+++ mesads/app/templatetags/selectattr.py
@@ -0,0 +1,10 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def selectattr(items, name):
+    """Equivalent of the Jinja2 filter selectattr."""
+    return [getattr(item, name) for item in items]
