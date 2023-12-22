9663fdd3c70c9d847f8f75cd54f1395b60bea39e
Julien Castets <castets.j@gmail.com>
2023-12-22 16:11:18 +0100

    Test /registre_vehicules_relais/consulter

--- mesads/vehicules_relais/test_views.py
+++ mesads/vehicules_relais/test_views.py
@@ -1,5 +1,7 @@
 from .unittest import ClientTestCase
 
+from mesads.fradm.models import Prefecture
+
 
 class TestIndexView(ClientTestCase):
     def test_200(self):
@@ -8,3 +10,30 @@ class TestIndexView(ClientTestCase):
         self.assertEqual(
             resp.headers["Location"], "/registre_vehicules_relais/consulter"
         )
+
+
+class TestSearchView(ClientTestCase):
+    def test_get(self):
+        resp = self.anonymous_client.get("/registre_vehicules_relais/consulter")
+        self.assertEqual(resp.status_code, 200)
+        self.assertFalse(resp.context["is_proprietaire"])
+
+        resp = self.proprietaire_client.get("/registre_vehicules_relais/consulter")
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(resp.context["is_proprietaire"])
+
+    def test_post(self):
+        prefecture = Prefecture.objects.filter(numero="35").get()
+
+        for client in (self.anonymous_client, self.proprietaire_client):
+            resp = client.post(
+                "/registre_vehicules_relais/consulter",
+                {
+                    "prefecture": prefecture.id,
+                },
+            )
+            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(
+                resp.headers["Location"],
+                "/registre_vehicules_relais/consulter/departements/35",
+            )
--- mesads/vehicules_relais/unittest.py
+++ mesads/vehicules_relais/unittest.py
@@ -1,5 +1,13 @@
-from mesads.users.unittest import ClientTestCase as BaseClientTestCase
+from mesads.fradm.unittest import ClientTestCase as BaseClientTestCase
+
+from mesads.vehicules_relais.models import Proprietaire
 
 
 class ClientTestCase(BaseClientTestCase):
-    pass
+    def setUp(self):
+        super().setUp()
+
+        self.proprietaire_client, self.proprietaire_user = self.create_client()
+
+        proprietaire = Proprietaire.objects.create(nom="Propriétaire")
+        proprietaire.users.set([self.proprietaire_user])
