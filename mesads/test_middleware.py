7f528ac36e480232f01f986765da972322385d39
Julien Castets <castets.j@gmail.com>
2024-01-12 12:59:32 +0100

    Test BackwardCompatibilityURLMiddleware

---
+++ mesads/test_middleware.py
@@ -0,0 +1,47 @@
+from django.http import HttpResponse
+from django.test import RequestFactory, TestCase
+
+from .middleware import BackwardCompatibilityURLMiddleware
+
+
+class TestBackwardCompatibilityURLMiddleware(TestCase):
+    def test_redirection(self):
+        factory = RequestFactory()
+
+        # These URLs exist with the prefix /registre_ads
+        for url in (
+            "/admin_gestion",
+            "/gestion",
+            "/gestion/123/",
+            "/gestion/3432/arrete",
+        ):
+            middleware = BackwardCompatibilityURLMiddleware(
+                lambda request: HttpResponse(status=404)
+            )
+            request = factory.get(url)
+            response = middleware(request)
+            self.assertEqual(response.status_code, 301)
+
+        # These URLs don't exist
+        for url in (
+            "/whatever",
+            "/xx",
+        ):
+            middleware = BackwardCompatibilityURLMiddleware(
+                lambda request: HttpResponse(status=404)
+            )
+            request = factory.get(url)
+            response = middleware(request)
+            self.assertEqual(response.status_code, 404)
+
+        # These URLs exist
+        for url in (
+            "/admin",
+            "/auth/login",
+        ):
+            middleware = BackwardCompatibilityURLMiddleware(
+                lambda request: HttpResponse(status=200)
+            )
+            request = factory.get(url)
+            response = middleware(request)
+            self.assertEqual(response.status_code, 200)
