3a53dc1a7761fde6d6367d620c24eb5823a192b5
Julien Castets <castets.j@gmail.com>
2024-06-01 21:19:35 +0200

    Fix tests

--- mesads/settings_test.py
+++ mesads/settings_test.py
@@ -3,6 +3,7 @@
 Make sure to reset production data that might have been retrieved from the
 environment. We do not want to store files in S3 or send emails in tests.
 """
+
 from .settings import *  # noqa
 
 
@@ -14,8 +15,11 @@ EMAIL_HOST_USER = None
 EMAIL_HOST_PASSWORD = None
 EMAIL_USE_TLS = None
 
-DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
-AWS_S3_ENDPOINT_URL = None
-AWS_S3_ACCESS_KEY_ID = None
-AWS_S3_SECRET_ACCESS_KEY = None
-AWS_STORAGE_BUCKET_NAME = None
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
