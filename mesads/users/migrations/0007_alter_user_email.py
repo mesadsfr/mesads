f7342a3201dbb10fae674cf9bc1552275e2c60a8
Julien Castets <castets.j@gmail.com>
2024-08-08 23:10:05 +0200

    Add missing migration

---
+++ mesads/users/migrations/0007_alter_user_email.py
@@ -0,0 +1,27 @@
+from django.contrib.postgres.operations import CreateCollation
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("users", "0006_remove_user_unique_ci_email"),
+    ]
+
+    operations = [
+        CreateCollation(
+            "case_insensitive",
+            provider="icu",
+            locale="und-u-ks-level2",
+            deterministic=False,
+        ),
+        migrations.AlterField(
+            model_name="user",
+            name="email",
+            field=models.TextField(
+                db_collation="case_insensitive",
+                error_messages={"unique": "Un utilisateur avec cet email existe déjà."},
+                unique=True,
+                verbose_name="email address",
+            ),
+        ),
+    ]
