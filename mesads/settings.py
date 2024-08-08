2ad5e6bf23a6ac8f8914f00cc2660f077e2dee5f
Julien Castets <castets.j@gmail.com>
2024-08-08 14:20:23 +0200

    Enable matomo

--- mesads/settings.py
+++ mesads/settings.py
@@ -41,6 +41,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = parse_env_bool("DEBUG", True)
 
+# Only enable matomo on production
+MESADS_STATS_ENABLED = not DEBUG
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
