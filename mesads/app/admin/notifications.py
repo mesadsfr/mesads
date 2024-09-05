6cb0d43d57645219f77877cee0318529a7f85c3d
Julien Castets <castets.j@gmail.com>
2024-09-05 16:01:33 +0200

    admin: search notifications by email

--- mesads/app/admin/notifications.py
+++ mesads/app/admin/notifications.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import F, Q
+from django.db.models.functions import Collate
 
 from mesads.app.models import Notification
 
@@ -11,3 +13,23 @@ class NotificationAdmin(admin.ModelAdmin):
         "user",
         "ads_manager_requests",
     )
+
+    search_fields = ("user__email",)
+
+    def get_search_results(self, request, queryset, search_term):
+        """The field Users.email uses a non-deterministic collation, which makes
+        it impossible to perform a LIKE query on it.
+
+        By overriding this method, we can specify the collation to use for the search.
+        """
+        use_distinct = True
+        queryset = queryset.annotate(collated_email=Collate(F("user__email"), "C"))
+        queryset = queryset.filter(
+            Q(
+                collated_email__icontains=search_term,
+            )
+        )
+        return (
+            queryset,
+            use_distinct,
+        )
