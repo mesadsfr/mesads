4f357b3e9578dd8bf67e8a69ee606294dfc80cfa
amichon <antoine@hashbang.coop>
2026-01-30 11:03:52 +0100

    feat: Rework du modèle users, mise a jours des tests

--- mesads/users/tests/test_models.py
+++ mesads/users/tests/test_models.py
@@ -1,114 +1,123 @@
 import datetime
 
+import pytest
 from dateutil.relativedelta import relativedelta
-from django.test import TestCase
 from django.utils import timezone
 
-from .factories import NoteUtilisateurFactory, UserAuditEntryFactory, UserFactory
+from ..models import User
+from .factories import NoteUtilisateurFactory, UserAuditEntryFactory
+
+pytestmark = pytest.mark.django_db
 
 
-class TestMethodShowNotation(TestCase):
-    def setUp(self):
-        super().setUp()
-        self.user = UserFactory()
+def test_show_notation_without_connexion(user: User):
+    assert not user.show_notation()
 
-    def test_show_notation_without_connexion(self):
-        self.assertFalse(self.user.show_notation())
 
-    def test_show_notation_with_one_connexion(self):
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        self.assertFalse(self.user.show_notation())
+def test_show_notation_with_one_connexion(user: User):
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    assert not user.show_notation()
 
-    def test_show_notation_with_more_than_one_connexion(self):
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        self.assertTrue(self.user.show_notation())
 
-    def test_show_notation_with_last_affichage_older_than_a_month(self):
-        last_affichage = timezone.now() - relativedelta(months=2)
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        NoteUtilisateurFactory(user=self.user, dernier_affichage=last_affichage.date())
-        self.assertTrue(self.user.show_notation())
+def test_show_notation_with_more_than_one_connexion(user: User):
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    assert user.show_notation()
 
-    def test_show_notation_with_last_affichage_younger_than_a_month(self):
-        last_affichage = timezone.now() - datetime.timedelta(days=10)
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        NoteUtilisateurFactory(user=self.user, dernier_affichage=last_affichage.date())
-        self.assertFalse(self.user.show_notation())
 
-    def test_show_notation_with_last_older_than_a_month_and_note_younger_than_six(self):
-        last_affichage = timezone.now() - relativedelta(months=2)
-        last_note = timezone.now() - relativedelta(months=2)
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        NoteUtilisateurFactory(
-            user=self.user,
-            dernier_affichage=last_affichage.date(),
-            derniere_note=last_note.date(),
-        )
-        self.assertFalse(self.user.show_notation())
+def test_show_notation_with_last_affichage_older_than_a_month(user: User):
+    last_affichage = timezone.now() - relativedelta(months=2)
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    NoteUtilisateurFactory(user=user, dernier_affichage=last_affichage.date())
+    assert user.show_notation()
 
-    def test_show_notation_with_last_older_than_a_month_and_note_older_than_six(self):
-        last_affichage = timezone.now() - relativedelta(months=2)
-        last_note = timezone.now() - relativedelta(months=7)
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        NoteUtilisateurFactory(
-            user=self.user,
-            dernier_affichage=last_affichage.date(),
-            derniere_note=last_note.date(),
-        )
-        self.assertTrue(self.user.show_notation())
 
-    def test_show_notation_with_last_younger_than_a_month_and_note_older_than_six(self):
-        last_affichage = timezone.now() - datetime.timedelta(days=10)
-        last_note = timezone.now() - relativedelta(months=7)
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        UserAuditEntryFactory(
-            user=self.user,
-            action="login",
-        )
-        NoteUtilisateurFactory(
-            user=self.user,
-            dernier_affichage=last_affichage.date(),
-            derniere_note=last_note.date(),
-        )
-        self.assertFalse(self.user.show_notation())
+def test_show_notation_with_last_affichage_younger_than_a_month(user: User):
+    last_affichage = timezone.now() - datetime.timedelta(days=10)
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    NoteUtilisateurFactory(user=user, dernier_affichage=last_affichage.date())
+    assert not user.show_notation()
+
+
+def test_show_notation_with_last_older_than_a_month_and_note_younger_than_six(
+    user: User,
+):
+    last_affichage = timezone.now() - relativedelta(months=2)
+    last_note = timezone.now() - relativedelta(months=2)
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    NoteUtilisateurFactory(
+        user=user,
+        dernier_affichage=last_affichage.date(),
+        derniere_note=last_note.date(),
+    )
+    assert not user.show_notation()
+
+
+def test_show_notation_with_last_older_than_a_month_and_note_older_than_six(user: User):
+    last_affichage = timezone.now() - relativedelta(months=2)
+    last_note = timezone.now() - relativedelta(months=7)
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    NoteUtilisateurFactory(
+        user=user,
+        dernier_affichage=last_affichage.date(),
+        derniere_note=last_note.date(),
+    )
+    assert user.show_notation()
+
+
+def test_show_notation_with_last_younger_than_a_month_and_note_older_than_six(
+    user: User,
+):
+    last_affichage = timezone.now() - datetime.timedelta(days=10)
+    last_note = timezone.now() - relativedelta(months=7)
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    UserAuditEntryFactory(
+        user=user,
+        action="login",
+    )
+    NoteUtilisateurFactory(
+        user=user,
+        dernier_affichage=last_affichage.date(),
+        derniere_note=last_note.date(),
+    )
+    assert not user.show_notation()
